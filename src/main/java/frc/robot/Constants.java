5d4a2581109ce4793491581cbdc8c1d1aa025952
ColeSaunders25 <colesaunders25@marist.com>
2023-06-28 14:30:09 -0400

    slider changes again

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -78,7 +78,7 @@ public final class Constants {
 
   }
   public static final class SliderConstants{
-    public static final double kP = .2;
+    public static final double kP = 1;
     public static final double kI = 0;
     public static final double kD = 0;
     public static final double kIz = 0;
--- src/main/java/frc/robot/subsystems/SliderSubsystem.java
+++ src/main/java/frc/robot/subsystems/SliderSubsystem.java
@@ -12,53 +12,46 @@ import frc.robot.Constants;
 public class SliderSubsystem extends SubsystemBase {
     
     
-    //private CANSparkMax rightSliderMotor;//Leader Motor
+    private CANSparkMax rightSliderMotor;//Leader Motor
     private CANSparkMax leftSliderMotor;
     //Only the Leader Actuator needs a PID Controller as the followers will mimic the leaders behaviour
-    // private SparkMaxPIDController leaderPIDController;
-    private SparkMaxPIDController followerPIDController;
-    // private RelativeEncoder rightSliderRelativeEncoder;
+     private SparkMaxPIDController leaderPIDController;
+     private RelativeEncoder rightSliderRelativeEncoder;
     private RelativeEncoder leftSliderRelativeEncoder;
 
 
 
     public SliderSubsystem(){
         //Instantiate the CAN Spark Maxes
-        //leftSliderMotor=new CANSparkMax(4, MotorType.kBrushless); // was 3
-        //rightSliderMotor = new CANSparkMax(4, MotorType.kBrushless);
+        leftSliderMotor=new CANSparkMax(3, MotorType.kBrushless); 
+        rightSliderMotor = new CANSparkMax(4, MotorType.kBrushless);
 
         //instantiate the leader's (right slider's) PID Controller
-        //leaderPIDController = rightSliderMotor.getPIDController();
-        //followerPIDController = leftSliderMotor.getPIDController();
+        leaderPIDController = rightSliderMotor.getPIDController();
 
         //Set PID Controller Variables
         //setting pid variables from constants class
-        // leaderPIDController.setP(Constants.SliderConstants.kP);
-        // leaderPIDController.setI(Constants.SliderConstants.kI);
-        // leaderPIDController.setD(Constants.SliderConstants.kD);
-        // leaderPIDController.setIZone(Constants.SliderConstants.kIz);
-        // leaderPIDController.setFF(Constants.SliderConstants.kFF);
-        // leaderPIDController.setOutputRange(Constants.SliderConstants.kMinOutput, Constants.SliderConstants.kMaxOutput);
+        leaderPIDController.setP(Constants.SliderConstants.kP);
+        leaderPIDController.setI(Constants.SliderConstants.kI);
+        leaderPIDController.setD(Constants.SliderConstants.kD);
+        leaderPIDController.setIZone(Constants.SliderConstants.kIz);
+        leaderPIDController.setFF(Constants.SliderConstants.kFF);
+        leaderPIDController.setOutputRange(Constants.SliderConstants.kMinOutput, Constants.SliderConstants.kMaxOutput);
         
-        // followerPIDController.setP(Constants.SliderConstants.kP);
-        // followerPIDController.setI(Constants.SliderConstants.kI);
-        // followerPIDController.setD(Constants.SliderConstants.kD);
-        // followerPIDController.setIZone(Constants.SliderConstants.kIz);
-        // followerPIDController.setFF(Constants.SliderConstants.kFF);
-        // followerPIDController.setOutputRange(Constants.SliderConstants.kMinOutput, Constants.SliderConstants.kMaxOutput);
+      
 
 
         //instaniate the Relative Encodoer for the slider Motors
-        //leftSliderRelativeEncoder = leftSliderMotor.getEncoder();
-        //rightSliderRelativeEncoder = rightSliderMotor.getEncoder();
+        leftSliderRelativeEncoder = leftSliderMotor.getEncoder();
+        rightSliderRelativeEncoder = rightSliderMotor.getEncoder();
 
         //set the leftSlider Motor to follow the leader(right) motor
-        //leftSliderMotor.follow(rightSliderMotor);
+       // leftSliderMotor.follow(rightSliderMotor);
     }
     @Override
     public void periodic(){
-        //SmartDashboard.putNumber("leftSliderEncoderPos", leftSliderRelativeEncoder.getPosition());
-        //SmartDashboard.putNumber("rightSliderEncoderPos", rightSliderRelativeEncoder.getPosition());
+        SmartDashboard.putNumber("leftSliderEncoderPos", leftSliderRelativeEncoder.getPosition());
+        SmartDashboard.putNumber("rightSliderEncoderPos", rightSliderRelativeEncoder.getPosition());
 
     }
     /**
@@ -67,15 +60,13 @@ public class SliderSubsystem extends SubsystemBase {
      * @param encoderCounts target encoder counts for the motor to run to
      */
     public void goToPos(double encoderCounts){
-        //leaderPIDController.setReference(encoderCounts, CANSparkMax.ControlType.kPosition);
-        //followerPIDController.setReference(encoderCounts, CANSparkMax.ControlType.kPosition);
-        //followerPIDController.setReference(10, CANSparkMax.ControlType.kPosition);
-        //leftSliderMotor.set(0.2);
+        leaderPIDController.setReference(encoderCounts, CANSparkMax.ControlType.kPosition);
+        //rightSliderMotor.set(.2);
     }
 
     public void resetEncoder(){
-        //leftSliderRelativeEncoder.setPosition(0);
-        //rightSliderRelativeEncoder.setPosition(0);
+        leftSliderRelativeEncoder.setPosition(0);
+        rightSliderRelativeEncoder.setPosition(0);
     }
 
     /**
