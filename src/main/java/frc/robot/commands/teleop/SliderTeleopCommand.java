013ba7a94f0631c198381fb2daa87c5122dc996a
nebomusic <michaudc@marist.com>
2023-06-28 13:15:18 -0400

    more slieder testing changes

--- src/main/java/frc/robot/commands/teleop/SliderTeleopCommand.java
+++ src/main/java/frc/robot/commands/teleop/SliderTeleopCommand.java
@@ -19,7 +19,9 @@ public class SliderTeleopCommand extends CommandBase{
 
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() {}
+    public void initialize() {
+      slider.resetEncoder();
+    }
 
   
     // Called every time the scheduler runs while the command is scheduled.
