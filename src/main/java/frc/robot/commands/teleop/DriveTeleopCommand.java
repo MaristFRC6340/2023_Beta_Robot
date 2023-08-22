27d57757f0038605acbb12926ba334de0f6dec04
nebomusic <michaudc@marist.com>
2023-08-22 16:42:41 -0400

    testing changes to fix robot drifting

--- src/main/java/frc/robot/commands/teleop/DriveTeleopCommand.java
+++ src/main/java/frc/robot/commands/teleop/DriveTeleopCommand.java
@@ -5,7 +5,10 @@
 package frc.robot.commands.teleop;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveSubsystem;
@@ -15,6 +18,9 @@ public class DriveTeleopCommand extends CommandBase {
   // Field for DriveSubsystem
   private final DriveSubsystem m_robotDrive;
   private  double manualSpeedModifier;
+
+  private double desiredHeading;
+  private PIDController thetaController;
 
 
   /** Creates a new DriveCommand. */
@@ -26,7 +32,13 @@ public class DriveTeleopCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    //set the deisred heading to the odometrys heading
+    desiredHeading = m_robotDrive.getPose().getRotation().getDegrees();
+    //intialize the PID Controller
+    thetaController = new PIDController(Constants.DriveConstants.thetaP, Constants.DriveConstants.thetaI, Constants.DriveConstants.thetaD);
+    thetaController.enableContinuousInput(0, 360);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -40,16 +52,43 @@ public class DriveTeleopCommand extends CommandBase {
     else{
       manualSpeedModifier=1;
     }
+
+    //update the theta Controller
+    thetaController.setSetpoint(desiredHeading);
+
+    if(Math.abs(MathUtil.applyDeadband(-Robot.getDriveControlJoystick().getRightX(), 0.06)) > 0){
+      desiredHeading += MathUtil.applyDeadband(Robot.getDriveControlJoystick().getRightX(), 0.06);
+    }
+
+    //wrap the values for the desired heading
+    if(desiredHeading > 360)desiredHeading = desiredHeading-360;
+    else if(desiredHeading <=0) desiredHeading = 360 + desiredHeading;
+
+
+
+    //get the power to run the rotational speed at
+    double rotPower = thetaController.calculate(m_robotDrive.getPose().getRotation().getDegrees());
+
+    //Send values to SmartDashboard
+    SmartDashboard.putNumber("rotPower", rotPower);
+    SmartDashboard.putNumber("desiredHeading", desiredHeading);
+
+
+
+
     // Updated Drive Command
     // Postive and Negative signs fixed for Remote input: michaudc 05 may 23
     m_robotDrive.drive(
                 MathUtil.applyDeadband(-Robot.getDriveControlJoystick().getLeftY()*DriveConstants.SpeedMultiplier*manualSpeedModifier, 0.06),
                 MathUtil.applyDeadband(-Robot.getDriveControlJoystick().getLeftX()*DriveConstants.SpeedMultiplier*manualSpeedModifier, 0.06),
                 MathUtil.applyDeadband(-Robot.getDriveControlJoystick().getRightX()*DriveConstants.SpeedMultiplier*manualSpeedModifier, 0.06),
+                /**rotPower,**/
               true);
 
     if(Robot.getDriveControlJoystick().getPOV()!=-1){
       m_robotDrive.zeroHeading(Robot.getDriveControlJoystick().getPOV());
+      //set the desired heading to be the POV
+      desiredHeading = Robot.getDriveControlJoystick().getPOV();
     }
 
   }
